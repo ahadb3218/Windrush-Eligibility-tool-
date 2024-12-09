32059d5aef3f2ed7c26d2991ec25d58f498f6281
ahadb3218 <ahadb3218@gmail.com>
2024-12-09 21:19:06 +0000

    tried paching email send again!

--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -1,7 +1,7 @@
 import { EmailConfig } from '../services/email/types';
 
 export const emailConfig: EmailConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || '',
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || '',
-  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || '',
+  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_cnf63v5',
+  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || 'template_kwgh4kp',
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || 's_OvNibJhRM9dbcrr',
 };
